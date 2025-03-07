d13b91161e81a1cc0548ce907a0d29e9bf450581
Jayklef <arhawhojerry@gmail.com>
2025-03-07 20:44:23 +0000

    initial project setup

---
+++ src/main/java/com/jayklef/smart_email_reply/SmartEmailReplyApplication.java
@@ -0,0 +1,13 @@
+package com.jayklef.smart_email_reply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartEmailReplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartEmailReplyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jayklef/smart_email_reply/SmartEmailReplyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayklef.smart_email_reply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartEmailReplyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
